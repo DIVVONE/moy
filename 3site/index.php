b574a1b3613328513085058bd2c0f458839254ab
DIVVONE <116506321+DIVVONE@users.noreply.github.com>
2022-10-23 19:01:39 +0300

    Add files via upload

---
+++ 3site/index.php
@@ -0,0 +1,112 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title >Сласти</title>
+	 <meta charset="utf-8">
+ <link rel="shortcut icon" href="css/9.png"
+<script language = javascript>
+</script>
+<link rel="stylesheet" href="css/zz.css">
+<form action="css/index.php" method="POST">
+<style>
+@import url('https://fonts.googleapis.com/css?family=Caveat&display=swap');
+
+
+</style>
+</head>
+<body background="">
+
+<div class="b-topbar">
+
+<header>
+
+<div id="cartInfo">
+
+</div>
+		<div class="lasuniken"></div>
+  <input type="checkbox" class="nectionupacipate" id="nectionupacipate">
+  <label for="nectionupacipate" class="sidebarIconToggle">
+  <div class="nackingasecu gerasilopdsnev geras-1"></div>
+  <div class="nackingasecu deryndsakols"></div>
+  <div class="nackingasecu gerasilopdsnev geras-2"></div>
+  </label>
+  
+  <div id="nasugaculad">
+  
+  <ul class="kingisakeading">
+<li><a href="glav.html" target="_blank">Главная</a></li>
+  <li><a href="tort.html" target="_blank">Торты</a></li>
+  <li><a href="kap.html" target="_blank">Капкейки</a></li>
+  <li><a href="glav.html" target="_blank">Раф</a></li>
+  <li><a href="glav.html" target="_blank">Рав</a></li>
+  <li><a href="glav.html" target="_blank">О нас</a></li>
+  <div id="cartInfo ">
+  
+</div>
+  </ul>
+  
+  </div>
+
+<div class="logo" href="glav.html"></div>
+
+</header>
+</div>
+
+<div id="bukvi">
+			<div class="text">
+				<h2>Изготовление тортов и пряников</h2>
+			</div>
+</div>
+<div class="container">
+<div class="bukvi2">
+
+	<h1 align="center" style="color:#FFE4C4">Торт</h1>
+	<p class="stroke" align="center" style="color:#FFE4C4" >Нужен торт на день рождение или просто не хватает сладкого? 
+ Для вашего внимания – огромный ассортимент тортов:шоколадные, сливочные,фруктовые торты.
+"Сласти" предлагает любые торты и пряники на заказ по вашим пожеланиям.</p>
+<div class="btn-block">
+					<a href="tort.html">Просмотр</a>
+				</div>
+	</div>
+<div class="bukvi3">
+<h1 align="center" style="color:#FFE4C4">Капкейки</h1>
+	<p class="stroke" align="center" style="color:#FFE4C4">Нужен торт на день рождение или просто не хватает сладкого? 
+ Для вашего внимания – огромный ассортимент тортов:шоколадные, сливочные,фруктовые торты.
+"Сласти" предлагает любые торты и пряники на заказ по вашим пожеланиям.</p>
+<div class="btn-block">
+					<a href="kap.html">Просмотр</a>
+				</div>
+	</div>
+<div class="bukvi4">
+	<h1 align="center" style="color:#FFE4C4">Раф</h1>
+	<p class="stroke" align="center" style="color:#FFE4C4">Нужен торт на день рождение или просто не хватает сладкого? 
+ Для вашего внимания – огромный ассортимент тортов:шоколадные, сливочные,фруктовые торты.
+"Сласти" предлагает любые торты и пряники на заказ по вашим пожеланиям.</p>
+<div class="btn-block">
+					<a href="">Просмотр</a>
+				</div>
+	</div>
+<div class="bukvi5">
+	<h1 align="center" style="color:#FFE4C4">Рав</h1>
+	<p class="stroke" align="center" style="color:#FFE4C4">Нужен торт на день рождение или просто не хватает сладкого? 
+ Для вашего внимания – огромный ассортимент тортов:шоколадные, сливочные,фруктовые торты.
+"Сласти" предлагает любые торты и пряники на заказ по вашим пожеланиям.</p>
+<div class="btn-block">
+					<a href="">Просмотр</a>
+				</div>
+	</div>
+	</div>
+ <footer class="footer">
+		<div class="copyright">
+		</div>
+		<div class="links">
+			<div class="social vk links-ico">
+   				<a href="#" target="_blank"><i class="fa fa-vk fa-1x"></i></a>    
+			</div>
+			<div class="social facebook links-ico">
+    			<a href="#" target="_blank"><i class="fa fa-facebook fa-1x"></i></a>    
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ 3site/kap.php
@@ -0,0 +1,135 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title >Сласти</title>
+	 <meta charset="utf-8">
+ <link rel="shortcut icon" href="css/9.png"
+<script language = javascript>
+</script>
+<link rel="stylesheet" href="css/zz1.css">
+<style>
+@import url('https://fonts.googleapis.com/css?family=Caveat&display=swap');
+</style>
+</head>
+<body background="">
+<div id="container">
+<div class="b-topbar">
+<header>
+<div id="cartInfo">
+
+</div>
+		<div class="lasuniken"></div>
+  <input type="checkbox" class="nectionupacipate" id="nectionupacipate">
+  <label for="nectionupacipate" class="sidebarIconToggle">
+  <div class="nackingasecu gerasilopdsnev geras-1"></div>
+  <div class="nackingasecu deryndsakols"></div>
+  <div class="nackingasecu gerasilopdsnev geras-2"></div>
+  </label>
+  <div id="nasugaculad">
+  <ul class="kingisakeading">
+<li><a href="glav.html" target="_blank">Главная</a></li>
+  <li><a href="tort.html" target="_blank">Торты</a></li>
+  <li><a href="kap.html" target="_blank">Капкейки</a></li>
+  <li><a href="kap.html" target="_blank">Раф</a></li>
+  <li><a href="kap.html" target="_blank">Рав</a></li>
+  <li><a href="kap.html" target="_blank">О нас</a></li>
+  <div id="cartInfo ">
+</div>
+  </ul>
+  </div>
+<div class="logo"></div>
+</header>
+</div>
+</div>
+<div id="bukvi">
+			<div class="text">
+				<h2>Капкейки</h2>
+			</div>
+</div>
+
+       
+
+                              <div class="container">
+<div class="product-item">
+  <img src="zxc/11.jpg">
+  <div class="product-list">
+    <h3>Капкейк</h3>
+      <span class="price"> 500 Р</span>
+      <a href="" class="button">В корзину</a>
+  </div>
+</div>
+
+	<div class="product-item">
+  <img src="zxc/12.jpg">
+  <div class="product-list">
+    <h3>Капкейк</h3>
+      <span class="price"> 500 Р</span>
+      <a href="" class="button">В корзину</a>
+  </div>
+</div>
+<div class="product-item">
+  <img src="zxc/13.jpg">
+  <div class="product-list">
+    <h3>Капкейк</h3>
+      <span class="price"> 500 Р</span>
+      <a href="" class="button">В корзину</a>
+  </div>
+</div>
+<div class="product-item">
+  <img src="zxc/14.jpg">
+  <div class="product-list">
+   <h3>Капкейк</h3>
+      <span class="price"> 500 Р</span>
+      <a href="" class="button">В корзину</a>
+  </div>
+</div>
+	</div>
+	<div class="container">
+	<div class="product-item">
+  <img src="zxc/15.jpg">
+  <div class="product-list">
+   <h3>Капкейк</h3>
+      <span class="price"> 500 Р</span>
+      <a href="" class="button">В корзину</a>
+  </div>
+</div>
+<div class="product-item">
+  <img src="zxc/16.jpg">
+  <div class="product-list">
+    <h3>Капкейк</h3>
+      <span class="price"> 500 Р</span>
+      <a href="" class="button">В корзину</a>
+  </div>
+</div>
+<div class="product-item">
+  <img src="zxc/17.jpg">
+  <div class="product-list">
+   <h3>Капкейк</h3>
+      <span class="price"> 500 Р</span>
+      <a href="" class="button">В корзину</a>
+  </div>
+</div>
+<div class="product-item">
+  <img src="zxc/18.jpg">
+  <div class="product-list">
+   <h3>Капкейк</h3>
+      <span class="price"> 500 Р</span>
+      <a href="" class="button">В корзину</a>
+  </div>
+</div>
+	</div>
+	</div>
+ <footer class="footer">
+		<div class="copyright">
+		</div>
+		<div class="links">
+			<div class="social vk links-ico">
+   				<a href="#" target="_blank"><i class="fa fa-vk fa-1x"></i></a>    
+			</div>
+			<div class="social facebook links-ico">
+    			<a href="#" target="_blank"><i class="fa fa-facebook fa-1x"></i></a>    
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
